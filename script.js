c27f2dd2a212c83c4301c52457e79712f242314a
Fazka <fazkasad@gmail.com>
2019-12-15 18:40:43 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: true
+};
+
+//console.log(person["isMarried"]);
+//console.log(person.age);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+//console.log(arr[0]);
+
+//let answer = confirm("Question?");
+//console.log(typeof(answer));
+
+//let answer1 = +prompt("Are u 18?", "Yes");
+//console.log(typeof(answer1));
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+//console.log(typeof(answer1));
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++ + "\n" + decr--);

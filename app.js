a750734bcea4280d8a9613c6e0f717c0dc7a9817
Fazil <fazkasad@gmail.com>
2020-01-02 20:39:38 +0400

    some commit

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-let money = +prompt("Enter monthly budget:", ''),
+let money, time;
+
+function start() {
     time = prompt("Enter the date using yyyy-mm-dd format:", '');
+
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Enter monthly budget:", '');
+    }
+}
+
+start();
 
 let appData = {
     budget: money,
@@ -7,51 +16,69 @@ let appData = {
     optionalexpenses: {},
     income: [],
     timeData: time,
-    savings: false
+    savings: true
 };
 
-let questionsFlag = false;
-
-/*while (!questionsFlag) {
+function chooseExpenses() {
     for (let i = 0; i < 2; i++) {
         let a = prompt("Enter current month's required expenses:", ''),
             b = prompt("How much will it cost:", '');
         if (typeof (a) != null && typeof (b) != null &&
-            a != '' && b != '' && a.length < 50) {
+            a != '' && b != '' && a.length < 50) { // returns exception when null.length
             console.log("Circle's stage " + i + ": success");
             appData.expenses[a] = b;
-            questionsFlag = true;
         } else {
             console.log("Circle's stage " + i + ": failure");
-            questionsFlag = false;
-            appData.expenses = {};
-            break;
+            i--;
         }
     }
-}*/
+}
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Enter current month's required expenses:", ''),
-        b = prompt("How much will it cost:", '');
-    if (typeof (a) != null && typeof (b) != null &&
-        a != '' && b != '' && a.length < 50) {
-        console.log("Circle's stage " + i + ": success");
-        appData.expenses[a] = b;
-    } else {
-        console.log("Circle's stage " + i + ": failure");
-        i--;
+function chooseOptExpenses () {
+    for (let i = 1; i <= 3; i++) {
+        let c = prompt("Enter current month's optional expenses:", '');
+
+        if (typeof (c) != null  && c != '' && c.length < 50) { // returns exception when null.length
+            console.log("2nd Circle's stage " + i + ": success");
+            appData.optionalExpenses[i] = c;
+        } else {
+            console.log("2nd Circle's stage " + i + ": failure");
+            i--;
+        }
     }
 }
 
-appData.moneyPerDay = appData.budget / 30;
-alert("Daily budget: " + appData.moneyPerDay);
+chooseExpenses();
+chooseOptExpenses();
 
-if (appData.moneyPerDay < 100) {
-    console.log("Minimal income level");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Average income level");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("High income level");
-} else {
-    console.log("Something went wrong");
+function detectDailyBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Daily budget: " + appData.moneyPerDay);
 }
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Minimal income level");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Average income level");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("High income level");
+    } else {
+        console.log("Something went wrong");
+    }
+}
+
+detectDailyBudget();
+detectLevel();
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let saved = +prompt("Enter the sum of the savings:", ''),
+            percent = +prompt("What is the rate % :", '');
+        appData.monthIncome = (saved / 100 / 12 * percent).toFixed();
+        alert("Ur deposit's monthly income: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
